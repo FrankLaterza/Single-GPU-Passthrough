e186cdc8f97af4bec15a794b82e4929258762bfa
FrankLaterza <57380745+FrankLaterza@users.noreply.github.com>
2022-10-10 13:49:44 -0400

    Add files via upload

---
+++ check_iommu_group.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for d in $(find /sys/kernel/iommu_groups/ -type l | sort -n -k5 -t/); do 
+    n=${d#*/iommu_groups/*}; n=${n%%/*}
+    printf 'IOMMU Group %s ' "$n"
+    lspci -nns "${d##*/}"
+done;
